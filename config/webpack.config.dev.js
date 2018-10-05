e38c0b73608f4f511924b3a48d76cdb080f410f6
Julio Pavón <julio@jpavon.com>
2018-10-05 23:52:37 +0200

    updates

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -135,8 +135,15 @@ module.exports = {
     // https://twitter.com/wSokra/status/969633336732905474
     // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
     splitChunks: {
-      chunks: 'all',
-      name: false,
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
     },
     // Keep the runtime chunk seperated to enable long term caching
     // https://twitter.com/wSokra/status/969679223278505985
